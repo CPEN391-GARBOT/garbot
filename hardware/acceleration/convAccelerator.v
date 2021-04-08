b06a0c76d40d0ac534869588879165e4d7d77477
Lance Holland <lanceholland01@gmail.com>
2021-04-08 04:35:26 -0600

    controllers finished

--- hardware/acceleration/convAccelerator.v
+++ hardware/acceleration/convAccelerator.v
@@ -1,49 +1,36 @@
 module convAccelerator(input clk, input reset, input [31:0] dataIn,
                         input dataValid, input filter, output [31:0] dataOut);
 
-  reg [4:0] count;
-  reg [63:0] product;
-  reg [31:0] shifted_product;
-  reg [31:0] sum;
+  reg [3:0] count;
+  reg signed [63:0] product;
+  reg signed [31:0] shifted_product;
+  reg signed [31:0] sum;
+  wire signed [31:0] dataInSigned;
   assign dataOut = sum;
+  assign dataInSigned = dataIn;
 
-  reg loadAct1, loadAct2, loadAct3, loadAct4, loadAct5, loadAct6, loadAct7,
-      loadAct8, loadAct9, loadFil1, loadFil2, loadFil3, loadFil4, loadFil5,
+  reg loadFil1, loadFil2, loadFil3, loadFil4, loadFil5,
       loadFil6, loadFil7, loadFil8, loadFil9;
-  wire [31:0] outAct1, outAct2, outAct3, outAct4, outAct5, outAct6, outAct7,
-      outAct8, outAct9, outFil1, outFil2, outFil3, outFil4, outFil5, outFil6,
+  wire signed [31:0] outFil1, outFil2, outFil3, outFil4, outFil5, outFil6,
       outFil7, outFil8, outFil9;
 
-  // Registers to store current activations that are being manipulated
-  reg32 regAct1(clk, reset, loadAct1, dataIn, outAct1);
-  reg32 regAct2(clk, reset, loadAct2, dataIn, outAct2);
-  reg32 regAct3(clk, reset, loadAct3, dataIn, outAct3);
-  reg32 regAct4(clk, reset, loadAct4, dataIn, outAct4);
-  reg32 regAct5(clk, reset, loadAct5, dataIn, outAct5);
-  reg32 regAct6(clk, reset, loadAct6, dataIn, outAct6);
-  reg32 regAct7(clk, reset, loadAct7, dataIn, outAct7);
-  reg32 regAct8(clk, reset, loadAct8, dataIn, outAct8);
-  reg32 regAct9(clk, reset, loadAct9, dataIn, outAct9);
-
   // Registers to store filter values (3x3 means 9 registers)
-  reg32 regFil1(clk, reset, loadFil1, dataIn, outFil1);
-  reg32 regFil2(clk, reset, loadFil2, dataIn, outFil2);
-  reg32 regFil3(clk, reset, loadFil3, dataIn, outFil3);
-  reg32 regFil4(clk, reset, loadFil4, dataIn, outFil4);
-  reg32 regFil5(clk, reset, loadFil5, dataIn, outFil5);
-  reg32 regFil6(clk, reset, loadFil6, dataIn, outFil6);
-  reg32 regFil7(clk, reset, loadFil7, dataIn, outFil7);
-  reg32 regFil8(clk, reset, loadFil8, dataIn, outFil8);
-  reg32 regFil9(clk, reset, loadFil9, dataIn, outFil9);
+  convReg32 regFil1(clk, reset, loadFil1, dataInSigned, outFil1);
+  convReg32 regFil2(clk, reset, loadFil2, dataInSigned, outFil2);
+  convReg32 regFil3(clk, reset, loadFil3, dataInSigned, outFil3);
+  convReg32 regFil4(clk, reset, loadFil4, dataInSigned, outFil4);
+  convReg32 regFil5(clk, reset, loadFil5, dataInSigned, outFil5);
+  convReg32 regFil6(clk, reset, loadFil6, dataInSigned, outFil6);
+  convReg32 regFil7(clk, reset, loadFil7, dataInSigned, outFil7);
+  convReg32 regFil8(clk, reset, loadFil8, dataInSigned, outFil8);
+  convReg32 regFil9(clk, reset, loadFil9, dataInSigned, outFil9);
 
   always @(posedge clk, negedge reset) begin
+    loadFil1 = 1'b0; loadFil2 = 1'b0; loadFil3 = 1'b0;
+    loadFil4 = 1'b0; loadFil5 = 1'b0; loadFil6 = 1'b0;
+    loadFil7 = 1'b0; loadFil8 = 1'b0; loadFil9 = 1'b0;
     if (!reset) begin
       // Reset all values to 0
-      loadAct1 = 1'b0; loadAct2 = 1'b0; loadAct3 = 1'b0; loadAct4 = 1'b0;
-      loadAct5 = 1'b0; loadAct6 = 1'b0; loadAct7 = 1'b0; loadAct8 = 1'b0;
-      loadAct9 = 1'b0; loadFil1 = 1'b0; loadFil2 = 1'b0; loadFil3 = 1'b0;
-      loadFil4 = 1'b0; loadFil5 = 1'b0; loadFil6 = 1'b0; loadFil7 = 1'b0;
-      loadFil8 = 1'b0; loadFil9 = 1'b0;
       count = 4'b0;
       product = 64'b0;
       shifted_product = 32'b0;
@@ -59,180 +46,143 @@ module convAccelerator(input clk, input reset, input [31:0] dataIn,
           end
         end
         4'b0001: begin
-          loadFil1 = 1'b0;
           if (dataValid) begin
             loadFil2 = 1'b1;
             count <= count + 1'b1;
           end
         end
         4'b0010: begin
-          loadFil2 = 1'b0;
           if (dataValid) begin
             loadFil3 = 1'b1;
             count <= count + 1'b1;
           end
         end
         4'b0011: begin
-          loadFil3 = 1'b0;
           if (dataValid) begin
             loadFil4 = 1'b1;
             count <= count + 1'b1;
           end
         end
         4'b0100: begin
-          loadFil4 = 1'b0;
           if (dataValid) begin
             loadFil5 = 1'b1;
             count <= count + 1'b1;
           end
         end
         4'b0101: begin
-          loadFil5 = 1'b0;
           if (dataValid) begin
             loadFil6 = 1'b1;
             count <= count + 1'b1;
           end
         end
         4'b0110: begin
-          loadFil6 = 1'b0;
           if (dataValid) begin
             loadFil7 = 1'b1;
             count <= count + 1'b1;
           end
         end
         4'b0111: begin
-          loadFil7 = 1'b0;
           if (dataValid) begin
             loadFil8 = 1'b1;
             count <= count + 1'b1;
           end
         end
         4'b1000: begin
-          loadFil8 = 1'b0;
           if (dataValid) begin
             loadFil9 = 1'b1;
             count <= count + 1'b1;
           end
         end
         default: begin
-          loadFil9 = 1'b0;
           count <= 4'b0000;
         end
       endcase
     end
     else begin
-      // Each clock cycle a new value is calculated and added to the sum
-      case(count)
+      case (count)
         4'b0000: begin
           if (dataValid) begin
-            sum = 32'b0;
-            loadAct1 = 1'b1;
+            product = dataInSigned * outFil1;
+            shifted_product = product >>> 24;
+            sum = shifted_product;
             count <= count + 1'b1;
           end
         end
         4'b0001: begin
-          loadAct1 = 1'b0;
           if (dataValid) begin
-            loadAct2 = 1'b1;
+            product = dataInSigned * outFil2;
+            shifted_product = product >>> 24;
+            sum = sum + shifted_product;
             count <= count + 1'b1;
           end
         end
         4'b0010: begin
-          loadAct2 = 1'b0;
           if (dataValid) begin
-            loadAct3 = 1'b1;
-            product = outAct1 * outFil1;
+            product = dataInSigned * outFil3;
             shifted_product = product >>> 24;
             sum = sum + shifted_product;
             count <= count + 1'b1;
           end
         end
         4'b0011: begin
-          loadAct3 = 1'b0;
           if (dataValid) begin
-            loadAct4 = 1'b1;
-            product = outAct2 * outFil2;
+            product = dataInSigned * outFil4;
             shifted_product = product >>> 24;
             sum = sum + shifted_product;
             count <= count + 1'b1;
           end
         end
         4'b0100: begin
-          loadAct4 = 1'b0;
           if (dataValid) begin
-            loadAct5 = 1'b1;
-            product = outAct3 * outFil3;
+            product = dataInSigned * outFil5;
             shifted_product = product >>> 24;
             sum = sum + shifted_product;
             count <= count + 1'b1;
           end
         end
         4'b0101: begin
-          loadAct5 = 1'b0;
           if (dataValid) begin
-            loadAct6 = 1'b1;
-            product = outAct4 * outFil4;
+            product = dataInSigned * outFil6;
             shifted_product = product >>> 24;
             sum = sum + shifted_product;
             count <= count + 1'b1;
           end
         end
         4'b0110: begin
-          loadAct6 = 1'b0;
           if (dataValid) begin
-            loadAct7 = 1'b1;
-            product = outAct5 * outFil5;
+            product = dataInSigned * outFil7;
             shifted_product = product >>> 24;
             sum = sum + shifted_product;
             count <= count + 1'b1;
           end
         end
         4'b0111: begin
-          loadAct7 = 1'b0;
           if (dataValid) begin
-            loadAct8 = 1'b1;
-            product = outAct6 * outFil6;
+            product = dataInSigned * outFil8;
             shifted_product = product >>> 24;
             sum = sum + shifted_product;
             count <= count + 1'b1;
           end
         end
         4'b1000: begin
-          loadAct8 = 1'b0;
           if (dataValid) begin
-            loadAct9 = 1'b1;
-            product = outAct7 * outFil7;
+            product = dataInSigned * outFil9;
             shifted_product = product >>> 24;
             sum = sum + shifted_product;
             count <= count + 1'b1;
           end
         end
-        4'b1001: begin
-          loadAct9 = 1'b0;
-          product = outAct8 * outFil8;
-          shifted_product = product >>> 24;
-          sum = sum + shifted_product;
-          count <= count + 1'b1;
-        end
-        4'b1010: begin
-          product = outAct9 * outFil9;
-          shifted_product = product >>> 24;
-          sum = sum + shifted_product;
-          count <= count + 1'b1;
-        end
         default: begin
-          if (dataValid) begin
-            count = 4'b0;
-          end
+          count = 4'b0;
         end
       endcase
     end
   end
 
-endmodule: convAccelerator
+endmodule
 
 // 32-bit register
-module reg32(input clk, input reset, input load,
+module convReg32(input clk, input reset, input load,
             input [31:0] in, output [31:0] out);
 
   reg [31:0] currVal;
@@ -240,11 +190,11 @@ module reg32(input clk, input reset, input load,
 
   always @(posedge clk, negedge reset) begin
     if (reset == 1'b0)
-      currVal <= 32'b00000000000000000000000000000000;
+      currVal <= 32'b0;
     else if (load == 1'b1)
       currVal <= in;
     else
       currVal <= out;
   end
 
-endmodule: reg32
+endmodule
---
+++ hardware/acceleration/maxController.v
@@ -0,0 +1,173 @@
+module maxController(input clk, input reset,
+                    // slave (CPU-facing)
+                    output slave_waitrequest,
+                    input [2:0] slave_address,
+                    input slave_read, output [31:0] slave_readdata,
+                    input slave_write, input [31:0] slave_writedata,
+                    // master (SDRAM-facing)
+                    input master_waitrequest,
+                    output [31:0] master_address,
+                    output master_read, input [31:0] master_readdata,
+                    output master_write, output [31:0] master_writedata);
+
+  reg [31:0] masterAddress, slaveReadData;
+  reg load0, load1, load2, load3, load4, dataValid, masterRead, masterWrite, slaveWaitRequest, waiter;
+  wire [31:0] word0, word1, word2, word3, word4;
+  reg [7:0] numRows, numCols, numLayer, numOps;
+  reg [2:0] presentState, nextState;
+  wire [63:0] opsPerLayer;
+
+  assign opsPerLayer = (word4 >>> 1) * (word4 >>> 1);
+  assign master_address = masterAddress * 3'b100;
+  assign master_read = masterRead;
+  assign master_write = masterWrite;
+  assign slave_waitrequest = slaveWaitRequest;
+  assign slave_readdata = slaveReadData;
+
+  `define Wait 3'b000
+  `define Go0 3'b001
+  `define Go1 3'b010
+  `define Go2 3'b011
+  `define Go3 3'b100
+  `define PreGo 3'b101
+
+  maxReg32 maxReg0(clk, reset, load0, slave_writedata, word0); // start if written to
+  maxReg32 maxReg1(clk, reset, load1, slave_writedata, word1); // address to read from
+  maxReg32 maxReg2(clk, reset, load2, slave_writedata, word2); // address to write to
+  maxReg32 maxReg3(clk, reset, load3, slave_writedata, word3); // # of layers
+  maxReg32 maxReg4(clk, reset, load4, slave_writedata, word4); // row length
+  maxAccelerator maxAcc(clk, reset, master_readdata, dataValid, master_writedata);
+
+  // Combinational logic
+  always @(*) begin
+    load0 = 1'b0; load1 = 1'b0; load2 = 1'b0; load3 = 1'b0; load4 = 1'b0;
+    slaveReadData = 32'b0;
+    if (slave_write) begin
+      case (slave_address)
+        3'b000: load0 = 1'b1;
+        3'b001: load1 = 1'b1;
+        3'b010: load2 = 1'b1;
+        3'b011: load3 = 1'b1;
+        3'b100: load4 = 1'b1;
+        default:;
+      endcase
+    end
+    else if (slave_read) begin
+      case (slave_address)
+        3'b000: slaveReadData = word0;
+        3'b001: slaveReadData = word1;
+        3'b010: slaveReadData = word2;
+        3'b011: slaveReadData = word3;
+        3'b100: slaveReadData = word4;
+        default: slaveReadData = 32'b0;
+      endcase
+    end
+  end
+
+  // State Logic
+  always @(*) begin
+    slaveWaitRequest = 1'b1;
+    masterRead = 1'b0;
+    masterWrite = 1'b0;
+    masterAddress = 32'b0;
+    dataValid = 1'b0;
+    case (presentState)
+      `Wait: begin
+        slaveWaitRequest = 1'b0;
+        if (load0)
+          nextState = `PreGo;
+        else
+          nextState = `Wait;
+      end
+      `Go0: begin
+        masterAddress = word1 + (numCols + 1'b1) + numRows * word4 + numLayer * word4 * word4;
+        masterRead = 1'b1;
+        dataValid = 1'b1;
+        nextState = `Go1;
+      end
+      `Go1: begin
+        masterAddress = word1 + numCols + (numRows + 1'b1) * word4 + numLayer * word4 * word4;
+        masterRead = 1'b1;
+        dataValid = 1'b1;
+        nextState = `Go2;
+      end
+      `Go2: begin
+        masterAddress = word1 + (numCols + 1'b1) + (numRows + 1'b1) * word4 + numLayer * word4 * word4;
+        masterRead = 1'b1;
+        dataValid = 1'b1;
+        nextState = `Go3;
+      end
+      `Go3: begin
+        masterAddress = word2 + numOps;
+        masterWrite = 1'b1;
+        if (numOps >= opsPerLayer * word3)
+          nextState = `Wait;
+        else
+          nextState = `PreGo;
+      end
+      `PreGo: begin
+        masterAddress = word1 + numCols + numRows * word4 + numLayer * word4 * word4;
+        masterRead = 1'b1;
+        dataValid = 1'b1;
+        nextState = `Go0;
+      end
+      default: nextState = `Wait;
+    endcase
+  end
+
+  // Sequential Logic
+  always @(posedge clk, negedge reset) begin
+    if (!reset) begin
+      waiter = 1'b0;
+      numRows = 8'b0;
+      numCols = 8'b0;
+      numLayer = 8'b0;
+      numOps = 8'b0;
+      presentState = `Wait;
+    end
+    else begin
+      if (nextState == `PreGo && presentState != `Wait) begin
+        numCols = numCols + 2'b10;
+        numOps = numOps + 1'b1;
+        if (numCols >= word4) begin
+          numRows = numRows + 2'b10;
+          numCols = 8'b0;
+          if (numRows >= word4) begin
+            numLayer = numLayer + 1'b1;
+            numRows = 8'b0;
+          end
+        end
+      end
+      if (nextState == `Wait) begin
+        numRows = 8'b0;
+        numCols = 8'b0;
+        numLayer = 8'b0;
+        numOps = 8'b0;
+      end
+      if (!master_waitrequest || presentState == `Wait) begin
+        if (waiter)
+          presentState <= nextState;
+        waiter = waiter + 1'b1;
+      end
+    end
+  end
+
+endmodule
+
+// 32-bit register
+module maxReg32(input clk, input reset, input load,
+            input [31:0] in, output [31:0] out);
+
+  reg [31:0] currVal;
+  assign out = currVal;
+
+  always @(posedge clk, negedge reset) begin
+    if (reset == 1'b0)
+      currVal <= 32'b00000000000000000000000000000000;
+    else if (load == 1'b1)
+      currVal <= in;
+    else
+      currVal <= out;
+  end
+
+endmodule
