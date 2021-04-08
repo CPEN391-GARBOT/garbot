3fd9ff14f69637b35ddcdad855f73eecf4561329
David Onak <54957341+DavidOnak@users.noreply.github.com>
2021-04-07 21:33:59 -0700

    Improved performance speed

--- firmware/sample_integration/python_file_reader.py
+++ firmware/sample_integration/python_file_reader.py
@@ -2,15 +2,44 @@ import os
 import cv2
 
 import numpy as np
-from fxpmath import Fxp
 
 IMG_SIZE = 128
 
+# Takes a floating point number and decimal places and returns a binary
+# fixed point representation
+def float_bin(number, places = 24):
+  
+    if number.is_integer() :
+        whole = int(number)
+        return f'{whole:07b}' + "000000000000000000000000"
+    whole, dec = str(number).split(".")
+  
+    whole = int(whole)
+    dec = int (dec)
+  
+    # Convert to 7 bit binary
+    res = f'{whole:07b}'
+  
+    for x in range(places):
+        whole, dec = str((decimal_converter(dec)) * 2).split(".")
+        dec = int(dec)
+        res += whole
+  
+    return res
+  
+def decimal_converter(num): 
+    while num > 1:
+        num /= 10
+    return num
+
+
 def load_file():
-    path = "/home/pi/Desktop/image.jpg"
+    path = "./download.jpg"
 
     #XxYx3 bgr image
     img_array = cv2.imread(path, cv2.IMREAD_COLOR)
+
+    print(len(img_array))
 
     #XxYx3 rgb image
     im = img_array.copy()
@@ -19,6 +48,7 @@ def load_file():
 
     #128x128x3 rgb image    
     new_array = cv2.resize(im, (IMG_SIZE, IMG_SIZE))
+    #formatted_array = cv2.resize(im, (IMG_SIZE, IMG_SIZE))
 
     #3x128x128 rgb image and linearized
     formatted_array = np.transpose(new_array, (2, 0, 1))
@@ -29,21 +59,17 @@ def load_file():
     print(len(flat_array))
 
 
-    for pixel in flat_array:
-        abs_bin = Fxp(abs(pixel), signed=False, n_word=31, n_frac=24)
-        
-        if pixel >= 0:
-            sign_extended = "0" + abs_bin.bin()
-        else:
-            sign_extended = "1" + abs_bin.bin()
-            
-        with open("photo.bin", "ab") as myfile:
+    with open("photo.bin", "ab") as myfile:
+        for pixel in flat_array:
+            test = float_bin(abs(pixel), 24)
+            if pixel >= 0:
+                sign_extended = "0" + test
+            else:
+                sign_extended = "1" + test
+                
             n = int(sign_extended, 2)
             data = n.to_bytes(4, "big")
             myfile.write(data)
 
-
-
-    
 
 load_file()
